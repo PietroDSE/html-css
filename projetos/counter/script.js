d9c7ad1b74ed1c2bc74891c43eae6f6ab3fc391f
Pietro d'Avila <dsescuderop@gmail.com>
2023-08-28 22:42:01 -0300

    tic tac toe

---
+++ projetos/counter/script.js
@@ -0,0 +1,13 @@
+let add = document.querySelector('input#add')
+let res = document.querySelector('input#res')
+let dec = document.querySelector('input#dec')
+let vis = document.querySelector('p#visor')
+
+let count = 0
+function update(){
+    
+}
+add.addEventListener("click", function(){
+    count = 0
+})
+
