bef8086cac834ed7d1fc0b2fa62268f32310b793
Pietro d'Avila <dsescuderop@gmail.com>
2023-08-28 22:46:12 -0300

    tic tac toe

--- projetos/tic tac toe/script.js
+++ projetos/tic tac toe/script.js
@@ -49,9 +49,9 @@ function handleClick(e) {
 
 function endGame(draw) {
   if (draw) {
-    winningMessageTextElement.innerText = 'Draw!'
+    winningMessageTextElement.innerText = 'Velha!'
   } else {
-    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    winningMessageTextElement.innerText = `${circleTurn ? "0's" : "X's"} Venceu!`
   }
   winningMessageElement.classList.add('show')
 }
