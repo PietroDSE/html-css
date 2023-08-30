eed93dcbe5cbfe15896ad6f56bba8255ec57fb1b
Pietro d'Avila <dsescuderop@gmail.com>
2023-08-30 20:38:56 -0300

    D&L

--- projetos/e-commerce-store/script.js
+++ projetos/e-commerce-store/script.js
@@ -1,20 +1,58 @@
-
-
 let countCart = 0;
-const cartIcon = document.getElementById('cart')
-const cartMenu = document.getElementById('cart-menu')
+const cartIcon = document.getElementById('cart');
+const cartMenu = document.getElementById('cart-menu');
+
 
 cartIcon.addEventListener("click", () => {
-    cartMenu.style.display = cartMenu.style.display === 'block' ? 'none' : 'block'
-})
+    cartMenu.style.display = cartMenu.style.display === 'block' ? 'none' : 'block';
+
+});
 
 function addToCart(productId) {
-    // Adicionar o produto ao carrinho
-    countCart++;
-    updateCount();
+    const product = getProductDetails(productId);
+    if (product) {
+        updateCartmenu(product);
+        countCart++;
+        updateCount();
+    }
+}
+
+function updateCartmenu(product){
+    const cartMenuElement = document.querySelector('div#cart-menu');
+    addElementToCart(product, cartMenuElement);
 }
 
 function updateCount() {
-    const cartCountElement = document.getElementById('cart-count');
+    const cartCountElement = document.querySelector('div#cart-count');
     cartCountElement.textContent = countCart;
+}
+
+function addElementToCart(product, cartMenuElement){
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart-menu-item');
+
+    const productImg = document.createElement('img');
+    productImg.src = product.imgSrc; // Assuming the "imgSrc" property contains the image URL
+    cartItem.appendChild(productImg);
+
+    const productName = document.createElement('p');
+    productName.textContent = product.name;
+    cartItem.appendChild(productName);
+
+    cartMenuElement.appendChild(cartItem);
+}
+
+function getProductDetails(productId) {
+    const productElement = document.querySelector(`div.product[data-product-id="${productId}"]`);
+
+    if (productElement) {
+        const productImg = productElement.querySelector('img');
+        const productName = productElement.querySelector('p');
+        return {
+            imgSrc: productImg.src,
+            name: productName.textContent,
+        };
+    }
+
+    return null;
 }
